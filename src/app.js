d46c853963774c66187580b46a20e0a21db936a4
Aanushkaaa <91602302+Aanushkaaa@users.noreply.github.com>
2022-05-01 15:51:05 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,50 +1,68 @@
-const PLAYERS = [
-    "Spiderman",
-    "Captain America",
-    "Wonderwoman",
-    "Popcorn",
-    "Gemwoman",
-    "Bolt",
-    "Antwoman",
-    "Mask",
-    "Tiger",
-    "Captain",
-    "Catwoman",
-    "Fish",
-    "Hulk",
-    "Ninja",
-    "Black Cat",
-    "Volverine",
-    "Thor",
-    "Slayer",
-    "Vader",
+@@ -21,38 +21,75 @@ const PLAYERS = [
     "Slingo"
 ];
+
 
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
+    for (var i = 0; i < players.length; i++) {
+        detailedPlayers[i] = {
+            name: players[i],
+            strength: getRandomStrength(),
+            image: "images/super-" + (i + 1) + ".png",
+            type: herovillian(i),
+        };
+    }
 
     return detailedPlayers;
 }
+
+
+function herovillian(n) {
+    if (n % 2 == 0) {
+        return "hero";
+    } else {
+        return "villain";
+    }
+}
+
 
 // getting random strength
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    var x = Math.ceil(Math.random() * 100);
+    return x;
 }
+
+
 
 const buildPlayers = (players, type) => {
     let fragment = '';
 
     // Loop through players and accumulate HTML template
+
     // depending of type of player(hero|villain)
     // Type your code here
 
+    for (var i = 0; i < players.length; i++) {
+        player = `<div class="player">
+                <img src="${players[i].image}" alt="">
+                <div class="name">${players[i].name}</div>
+                <div class="strength">${players[i].strength}</div>
+                </div>`;
+        if (players[i].type == type) {
+            fragment += player;
+        }
+        // return fragment;
+    }
     return fragment;
 }
+
+
 // Display players in HTML
 const viewPlayers = (players) => {
 
@@ -53,6 +71,8 @@ const viewPlayers = (players) => {
 
 }
 
+
 window.onload = () => {
     viewPlayers(initPlayers(PLAYERS));
+} 
 }
